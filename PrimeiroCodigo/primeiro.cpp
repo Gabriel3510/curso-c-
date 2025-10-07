c2b9123a657c9dd9d109c311d3dcb6258ad14849
Gabrielaero <gabriel.lima@speedbird.aero>
2025-10-06 22:40:55 -0300

    Primeiro codigo em C++

--- PrimeiroCodigo/primeiro.cpp
+++ PrimeiroCodigo/primeiro.cpp
@@ -5,6 +5,12 @@
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    //cou comando de saida de dados
+    //std::cout representa o fluxo de saída padrão (console)
+    //endl insere uma nova linha e
+    //garante que o buffer seja esvaziado
     return 0;
+    //retorna 0 para indicar que o programa terminou com sucesso
+    
 }
 
