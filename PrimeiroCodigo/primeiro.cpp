1ede9ae656ad81aec608269f818595080a715c74
Gabrielaero <gabriel.lima@speedbird.aero>
2025-10-06 22:39:36 -0300

    Primeiro codigo em C++

---
+++ PrimeiroCodigo/primeiro.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+// a diretiva de pro-processador #include
+//indica para incluir o conteúdo do arquivo
+//iostream (input-output stream) que contém
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
+
