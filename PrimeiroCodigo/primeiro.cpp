d9e2af24c23bd2f194b858036750c33c4f978d2e
Gabriel Lima <gabriel.lima@speedbird.aero>
2025-10-08 16:28:39 -0300

    My fist program

--- PrimeiroCodigo/primeiro.cpp
+++ PrimeiroCodigo/primeiro.cpp
@@ -1,16 +1,11 @@
 #include <iostream>
-// a diretiva de pro-processador #include
-//indica para incluir o conteúdo do arquivo
-//iostream (input-output stream) que contém
-
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+//A biblioteca iostream fornece funcionalidades para entrada e saída de dados, como os objetos std::cout (para imprimir na tela) e std::cin (para ler do teclado).
+ int main() {
+     std::cout << "Hello, World!" << std::endl;
     //cou comando de saida de dados
     //std::cout representa o fluxo de saída padrão (console)
     //endl insere uma nova linha e
     //garante que o buffer seja esvaziado
     return 0;
     //retorna 0 para indicar que o programa terminou com sucesso
-    
 }
-
---
+++ Project1/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    int favoriteNumber;
+
+    std::cout << "Enter your favorite number: ";
+
+    std::cin >> favoriteNumber;
+
+    std::cout<< "Your favorite number is: "<<std::endl;
+    
+    return 0;
+
+
+}
